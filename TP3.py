09ce22061d8b3dfe2ce5c067b8e92ae843247deb
elcusson <louiscusson1994@gmail.com>
2017-06-14 17:06:25 -0400

    J'ai ajouté les attributs des classes personnages et guerriers

--- TP3.py
+++ TP3.py
@@ -43,3 +43,29 @@ bouton5.pack(side = TOP)
 
 # Activation de la méthode mainloop() pour garder le programme actif en permanence
 fenetre1.mainloop()
+
+## Definition de la classe Personnage
+
+class Personnage: #attributs de la classe personnage
+    energie_depart_defaut = 20
+    energie_depart_min = 1
+    energie_max = 100
+    longueur_nom_min = 3
+    longueur_nom_max = 30
+    nom= ""
+    energie_depart = 0
+    energie_courante = 0
+
+
+
+### Définition de la classe Guerrier
+
+class Guerrier: ## les attributs de la classe guerrier
+    force_defaut = 20
+    force_max = 80
+    perte_force_defaut=2
+    gain_force_defaut = 10
+    force = 0
+
+
+
