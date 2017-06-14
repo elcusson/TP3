259d4ee6441b11e3cd9a3b0ff620ec39cfddfbda
ntonyproduction <gagnonanthony@hotmail.com>
2017-06-13 23:47:31 -0400

    J'ai rajouté les boutons de l'interface.

--- TP3.py
+++ TP3.py
@@ -8,16 +8,24 @@ from tkinter import *
 fenetre1 = Tk()
 
 # Création d'un objet/widget avec la classe Label()
-texte1 = Label(fenetre1, text = "Bonjour les copains!", fg = "red")
-
-# Activation de la méthode pack()
-texte1.pack()
+texte1 = Label(fenetre1, text = "La fenêtre principal se tient ici.", fg = "red")
+texte1.pack(side = LEFT) # Activation de la méthode pack()
 
 # Création d'un objet/widget avec la classe Button()
-bouton1 = Button(fenetre1, text = "Quitter", command = fenetre1.destroy)
+bouton1 = Button(fenetre1, text = "Créer un sorcier", width = 15)
+bouton1.pack(side = TOP) # Activation de la méthode pack()
 
-# Activation de la méthode pack()
-bouton1.pack()
+bouton2 = Button(fenetre1, text = "Créer un guerrier", width = 15)
+bouton2.pack(side = TOP)
+
+bouton3 = Button(fenetre1, text = "Attaquer", width = 15)
+bouton3.pack(side = TOP)
+
+bouton4 = Button(fenetre1, text = "Réinitialiser l'énergie", width = 15)
+bouton4.pack(side = TOP)
+
+bouton5 = Button(fenetre1, text = "Crier", width = 15)
+bouton5.pack(side = TOP)
 
 # Activation de la méthode mainloop() pour garder le programme actif en permanence
 fenetre1.mainloop()
